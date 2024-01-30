3c3828708c44bd6310f21b239b75cb269681bc93
znataniel <nrojaszavalla@frba.utn.edu.ar>
2024-01-30 16:53:08 -0300

    first real commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# DoMe! | Simple to-do app
+
+DoMe! is a simple to-do app featuring local cached storage and,
+options for different projects, and objectives with their
+corresponding title, description, due date and priority.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Bella Vita Ristorante",
+      template: "./src/template.html",
+    }),
+  ],
+};
