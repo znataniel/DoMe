d4602e30cd18d4db2d3a1eaeb55799e38b8eb860
znataniel <nrojaszavalla@frba.utn.edu.ar>
2024-02-04 00:15:37 -0300

    advance project ff & create test dummy function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
 import "./style.css";
+import "./projects.js";
+import ToDo from "./to-do.js";
+import Project from "./projects.js";
+
+function loadDummyProjects() {
+  const proj = Project("Home", new Array());
+  const todos = [
+    ToDo(
+      "Cook dinner",
+      "Cook dinner for George's visit.",
+      new Date(2023, 10, 2),
+    ),
+    ToDo(
+      "Clean mess",
+      "You are going to leave a mess with George. Make sure to clean afterwards.",
+      new Date(2023, 10, 3),
+    ),
+    ToDo(
+      "Kramer's vacuum cleaner",
+      "Get Martin to return Kramer's vacuum cleaner.",
+      new Date(2023, 10, 3),
+    ),
+  ];
+  todos.forEach((todo) => {
+    proj.addToDo(todo);
+  });
+  return proj;
+}
+
+const projectArr = new Array();
+projectArr.push(loadDummyProjects());
+
+projectArr.forEach((proj) => {
+  console.log(proj.getTitle());
+  proj.getToDoArr().forEach((todo) => {
+    console.log(
+      `>> ${todo.getState()} | ${todo.getTitle()} | ${todo.getDesc()} | ${todo.getPriority()} | ${todo.getDueDate()}`,
+    );
+  });
+});
--- src/projects.js
+++ src/projects.js
@@ -1,7 +1,19 @@
-export default function Project(title, todoArr) {
+export default function Project(title, toDos) {
+  const todoArr = toDos.flat();
+
   const getTitle = () => title;
   const setTitle = (newTitle) => {
-    title = String(newTitle);
+    title = newTitle.toString();
   };
-  return { getTitle, setTitle, todoArr };
+  const getToDoArr = () => todoArr;
+  const addToDo = (todo) => {
+    todoArr.push(todo);
+  };
+  const removeToDo = (todo) => {
+    const removeIndex = todoArr.indexOf(todo);
+    if (removeIndex > -1) {
+      todoArr.splice(removeIndex, 1);
+    }
+  };
+  return { getTitle, setTitle, getToDoArr, addToDo, removeToDo };
 }
