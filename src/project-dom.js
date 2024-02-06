991ec66dc349da841e110581c7223001758a2744
znataniel <nrojaszavalla@frba.utn.edu.ar>
2024-02-06 17:33:49 -0300

    add project title fn for project tab

---
+++ src/project-dom.js
@@ -0,0 +1,9 @@
+const createTitle = function (project) {
+  const title = document.createElement("h2");
+  title.textContent = project.getTitle();
+  return title;
+};
+
+const createToDoWrapper = function (project) {
+  const wrapper = document.createElement("div");
+};
