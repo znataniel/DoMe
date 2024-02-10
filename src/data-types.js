c893df5c957566a5b29c0d6e1873d81e36aa6a8b
znataniel <nrojaszavalla@frba.utn.edu.ar>
2024-02-09 23:49:01 -0300

    cleanup pt 2

--- src/data-types.js
+++ src/data-types.js
@@ -1,10 +1,4 @@
-export default function ToDo(
-  title,
-  desc,
-  dueDate,
-  priority = 1,
-  state = false,
-) {
+function ToDo(title, desc, dueDate, priority = 1, state = false) {
   state = Boolean(state);
 
   const priorities = ["low", "normal", "high"];
@@ -52,3 +46,24 @@ export default function ToDo(
     setPriority,
   };
 }
+function Project(title, toDos) {
+  const todoArr = toDos.flat();
+
+  const getTitle = () => title;
+  const setTitle = (newTitle) => {
+    title = newTitle.toString();
+  };
+  const getToDoArr = () => todoArr;
+  const addToDo = (todo) => {
+    todoArr.push(todo);
+  };
+  const removeToDo = (todo) => {
+    const removeIndex = todoArr.indexOf(todo);
+    if (removeIndex > -1) {
+      todoArr.splice(removeIndex, 1);
+    }
+  };
+  return { getTitle, setTitle, getToDoArr, addToDo, removeToDo };
+}
+
+export default { ToDo, Project };
